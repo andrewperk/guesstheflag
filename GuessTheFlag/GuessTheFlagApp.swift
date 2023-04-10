8f6d9c52eb891fb533beb59dca3a8567667aa7d5
Andrew Perkins <andrewperk@gmail.com>
2023-04-10 11:21:08 -0700

    first commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andrew L Perkins on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
